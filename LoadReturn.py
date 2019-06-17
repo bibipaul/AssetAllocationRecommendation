3bc1688120084906bbf4d33de9a52fd0bc6d11c8
bibipaul <47771936+bibipaul@users.noreply.github.com>
2019-06-17 21:58:12 +0100

    Update LoadReturn.py

--- LoadReturn.py
+++ LoadReturn.py
@@ -4,14 +4,14 @@ import os
 
 class LoadReturnData:
     def ImportDailyReturn():
-        quandl.ApiConfig.api_key='8xsHs-1vCFmLsqD5Sfpr'
+        quandl.ApiConfig.api_key='Paste Your quandl Key'
         stocks =['AAPL','AMZN','GOOGL','FB']
         data =quandl.get_table('WIKI/PRICES', ticker=stocks, qopts={'columns': ['date', 'ticker', 'adj_close']}
                       , date ={'gte': '2018-1-1', 'lte': '2018-12-31'}, paginate=True)
-        data.to_csv(r'C:\Users\Paul\Desktop\AI\Python\Learning\AssetAllocationRecommendation\ReturnData\DailyReturn.xls')
+        data.to_csv(r'$your path\DailyReturn.xls')
             
     def loadDailyReturn():
-        csv_path =os.path.join("C:/Users/Paul/Desktop/AI/Python/Learning/AssetAllocationRecommendation/ReturnData","DailyReturn.xls")
+        csv_path =os.path.join("$your path","DailyReturn.xls")
         df = pd.read_csv(csv_path)        
         return df
         
